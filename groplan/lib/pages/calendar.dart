e6a93d26d927ab5f496f36f1437ed459df8f1baa
ojasvinaik13 <ojasvi.naik@gmail.com>
2020-10-21 23:40:13 +0530

    Added Table_Calendar on Calendar Page

--- groplan/lib/pages/calendar.dart
+++ groplan/lib/pages/calendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   @override
@@ -6,17 +7,47 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  CalendarController _calendarController;
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+  }
+
+  Widget _buildTableCalendar() {
+    return TableCalendar(
+      startDay: DateTime.now(),
+      calendarController: _calendarController,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      calendarStyle: CalendarStyle(
+        selectedColor: Color(0xFF72C077),
+        todayColor: Color(0xFF72C077),
+        markersColor: Colors.brown[700],
+        outsideDaysVisible: false,
+      ),
+      headerStyle: HeaderStyle(
+        formatButtonTextStyle:
+            TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
+        formatButtonDecoration: BoxDecoration(
+          color: Color(0xFF72C077),
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Calendar'),
       ),
-      body: const Center(
-        child: Text(
-          'This is the calendar page',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: Center(
+        child: _buildTableCalendar(),
+        // Text(
+        //   'This is the calendar page',
+        //   style: TextStyle(fontSize: 24),
+        // ),
       ),
     );
   }
